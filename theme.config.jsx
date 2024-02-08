8409e636bed6b69d847d3b71322a577b25527a7b
Eddie Liao <eddieliao03@gmail.com>
2024-02-07 18:26:38 -0600

    Add starter files

---
+++ theme.config.jsx
@@ -0,0 +1,10 @@
+export default {
+    logo: <span>Stampede Docs</span>,
+    project: {
+      link: 'https://github.com/ut-ras/robomaster'
+    },
+    project: {
+        link: 'https://github.com/ut-ras/robomaster'
+    }
+    // ... other theme options
+  }
