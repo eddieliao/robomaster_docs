17cd880047c72daa2f1126700a070aa2569b26c6
Eddie Liao <eddieliao03@gmail.com>
2024-02-07 19:13:47 -0600

    Add SEO props

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,10 +1,18 @@
+import { useRouter } from 'next/router'
+
 export default {
     logo: <span>Stampede Docs</span>,
+    docsRepositoryBase: 'https://github.com/eddieliao/robomaster_docs',
     project: {
       link: 'https://github.com/ut-ras/robomaster'
     },
-    project: {
-        link: 'https://github.com/ut-ras/robomaster'
+    useNextSeoProps() {
+        const { asPath } = useRouter()
+        if (asPath !== '/') {
+          return {
+            titleTemplate: '%s – Stampede Docs'
+          }
+        }
     }
     // ... other theme options
-  }
+}
