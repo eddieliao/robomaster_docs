f95105ee6317880beba5a4ad74cf297db6068ee3
Eddie Liao <eddieliao03@gmail.com>
2024-02-08 16:20:23 -0600

    Add conditional basePath and assetPrefix

--- next.config.js
+++ next.config.js
@@ -8,6 +8,8 @@ module.exports = withNextra({
         unoptimized: true,
     },
     output: 'export',
+    basePath: process.env.NODE_ENV === "production" ? "/robomaster_docs/" : undefined,
+    assetPrefix: process.env.NODE_ENV === "production" ? "/robomaster_docs/" : undefined,
 })
    
 // If you have other Next.js configurations, you can pass them as the parameter:
